0655bf5a990a43b9d8b16d3febe501f04978831a
RodrigoVJ28-11-12 <rodrigovitoriojoao@gmail.com>
2025-10-23 18:10:17 -0300

    Implement Discord bot with message handling
    Replaced password generation code with a Discord bot implementation that responds to messages.

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import random
+import discord
 
-codes = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+# Ativar a permissão para ler o conteúdo das mensagens
+intents.message_content = True
+# Criar um bot e passar as permissões
+client = discord.Client(intents=intents)
 
-lengthinserting = int(input("insert length of the password"))
+@client.event
+async def on_ready():
+    print(f'Fizemos login como {client.user}')
 
-password = ""
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('hello'):
+        await message.channel.send("Hello!")
+    elif message.content.startswith('bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('this bot sucks'):
+        await message.channel.send("no u")
+    else:
+        await message.channel.send(message.content)
 
-for i in range(lengthinserting):
-
-    password += random.choice(codes)
-
-print(password)
+client.run("MTQyODQ4ODMxMzMxNjE4MDEyOA.GomAZp.CqD958zyO5fSbGYF49OWtVej9e8KAPwmELsQ0o")
